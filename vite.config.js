2b1b494ac71f9b3ce2c09648a46ef1e9255f4861
Timo Nielsen <timo@timonielsen.com>
2025-06-03 15:31:58 +0100

    disabling CSS source maps in development mode

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     port: 3000,
   },
   css: {
-    devSourcemap: true,
+    devSourcemap: false,
   },
   optimizeDeps: {
     exclude: ['js/vendor/**', 'css/vendor/**'],
